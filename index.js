28cea9a9c4bce41f1805f72f7d89e981a8ca66a4
muthyaldev123 <pulikantimuthyalreddy@gmail.com>
2024-08-27 21:51:43 +0530

    methods

---
+++ index.js
@@ -0,0 +1,62 @@
+// let arr=[1,2,3,4,5]
+// let x=arr.map((x)=>{return x*2});
+// console.log(x);
+
+// let arr=["hello","word"]
+// let z=arr.map((x)=>{return x.toUpperCase()});
+// console.log(z);
+
+// let arr=[1,2,3,4,5]
+// let z=arr.filter((x)=>{return x%2==0});
+// console.log(z);
+
+// let arr1=[1,2,3,4,5];
+// let arr2=arr1.filter(rock)
+// function rock(a){
+//     return a%2==0
+// }
+// console.log(arr2);
+
+// let arr=["hello","world","abc"]
+// let arr1=arr.filter((x)=>{return x.length-1>3})
+// console.log(arr1);
+
+// let arr=[1,2,3,4,5]
+// let arr1=arr.forEach((a)=>{return console.log(a)});
+
+// let arr=["hello","world"]
+// let arr1=arr.forEach((x)=>console.log(x+"!"))
+
+// let arr=[1,2,3,4,5]
+// let arr1=arr.every((a)=>{return a<=5})
+// console.log(arr1)
+
+// let arr=["hello","world","abc"]
+// let arr1=arr.every((a)=>{return a.length<5})
+// console.log(arr1)
+
+// let arr=[1,2,3,4,5]
+// let arr1=arr.some((a)=>{return a%2==0})
+// console.log(arr1)
+ 
+
+// let arr=["hello","world","max"]
+// let arr1=arr.some((a)=>{return a.length>4})
+// console.log(arr1)
+
+// let arr=[1,2,3,4,5]
+// let arr1=arr.find((a)=>{return a%2==0})
+// console.log(arr1)
+
+// let arr=["hello","world","max"]
+// let arr1=arr.find((z)=>{return z.length-1<3})
+// console.log(arr1)
+
+let arr=[1,2,3,4,5]
+let arr1=arr.findIndex((z)=>{return z%2==0})
+console.log(arr1)
+
+// let arr=["hello","world","max"]
+// let arr1=arr.findIndex((x)=>{return x.length==3})
+// console.log(arr1) 
+
